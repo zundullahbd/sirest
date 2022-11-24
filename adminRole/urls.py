bb67d707a2f4cc423ce830246ec680deeaa71bee
Nafhan Raissa Syandana <nafhan.raissa@ui.ac.id>
2022-11-24 14:34:18 +0700

    views urls fitur 13-15

--- adminRole/urls.py
+++ adminRole/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib import admin
+from .views import *
 
 app_name = 'adminRole'
 
@@ -9,6 +10,12 @@ urlpatterns = [
     path('daftar-tarif-pengiriman/', views.dtp, name='daftar-tarif-pengiriman'),
     path('update-tarif-pengiriman/', views.utp, name='update-tarif-pengiriman'),
     path('', views.dashAdmin, name='dashboard-admin'),
+    path('create-promo/', views.cp, name='create-promo'),
+    path('create-promo/min-transaksi.', views.fmt, name='create-promo-mintransaksi'),
+    path('create-promo/hari-spesial.', views.fhs, name='create-promo-harispesial'),
+    path('promo/', get_promo, name="get_promo"),
+    path('promo/detail-hari-spesial', get_detail_hariSpesial, name="get_detail_hariSpesial"),
+    path('promo/detail-min-transaction', get_detail_minTransaction, name="get_detail_minTransaction"),
 
 
 
